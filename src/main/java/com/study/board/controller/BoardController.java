8747d1b5376c35e5aadcc5546747392be23d1f6f
ksungsu <zxzxzlxlzlxl@naver.com>
2022-11-26 12:43:55 +0900

    혼자서 스프링 게시판 구현해보기 페이징

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.controller;
 import com.study.board.entity.SpringBoard;
 import com.study.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,8 +37,19 @@ public class BoardController {
     }
 
     @GetMapping("/boardList")
-    public String boardList(Model model){
-        model.addAttribute("list", boardService.boardList());
+    public String boardList(Model model , @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+
+        Page<SpringBoard> list = boardService.boardList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage-4, 1);
+        int endPage = Math.min(nowPage+5, list.getTotalPages());
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "boardList";
     }
 
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.study.board.service;
 import com.study.board.entity.SpringBoard;
 import com.study.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -34,9 +36,8 @@ public class BoardService {
         boardRepository.save(springBoard);
     }
 
-    public List<SpringBoard> boardList(){
-        List<SpringBoard> boardList =  boardRepository.findAll();
-        return boardList;
+    public Page<SpringBoard> boardList(Pageable pageable){
+        return boardRepository.findAll(pageable);
     }
 
     public SpringBoard boardView(Integer id){
